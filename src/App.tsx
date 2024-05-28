ddcd0b5ec008b2e1c7627ed622bbc9e3bb1c3c87
Bertrand <bertrandrigal.pro@gmail.com>
2024-05-28 17:05:05 +0200

    Add WalletConnect / wagmi / viem and use the connected address

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "@covalenthq/goldrush-kit/styles.css";
 
-import { useState } from 'react'
+import { useAccount } from "wagmi";
+
 import { TokenBalancesList } from "@covalenthq/goldrush-kit";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { address } = useAccount();
+  if (address) console.log(address);
 
   return (
     <>
@@ -15,26 +16,18 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <h1>Trading bro</h1>
+      {!address && (
+        <p className="read-the-docs">
+          Connect your wallet to see your token balances.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <TokenBalancesList
-        chain_names={[
+      )}
+      <w3m-button />
+      {address && (
+        <TokenBalancesList
+          chain_names={[
             "eth-mainnet",
-            "btc-mainnet",
             "matic-mainnet",
             "bsc-mainnet",
             "avalanche-mainnet",
@@ -43,12 +36,13 @@ function App() {
             "fantom-mainnet",
             "base-mainnet",
             "mantle-mainnet",
-        ]}
-        hide_small_balances
-        address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de" //sample address
-      />
+          ]}
+          hide_small_balances
+          address={address}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { GoldRushProvider } from "@covalenthq/goldrush-kit";
-import App from './App.tsx'
-import './index.css'
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { arbitrum, mainnet } from "wagmi/chains";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
+const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
+const wagmiMetadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "http://localhost:5173/", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum] as const;
+const config = defaultWagmiConfig({
+  chains,
+  projectId: walletConnectProjectId,
+  metadata: wagmiMetadata,
+});
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId: walletConnectProjectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoldRushProvider apikey={import.meta.env.VITE_COVALENT_API_KEY}>
-      <App />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
     </GoldRushProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
