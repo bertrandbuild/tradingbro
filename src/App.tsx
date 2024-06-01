270aed6985adbc4f4b4deaa8c1a1761823d9dc19
Bertrand <bertrandrigal.pro@gmail.com>
2024-06-01 14:04:02 +0200

    New api hooks

--- src/App.tsx
+++ src/App.tsx
@@ -6,93 +6,8 @@ import Loading from "./Components/Loading";
 import Chat from "./Components/Chat/Chat";
 import { useChatHook } from "./Components/Chat";
 import { useEffect } from "react";
-import Fuse from 'fuse.js'
-
-const isDevMode = import.meta.env.DEV;
-const networks = isDevMode
-  ? ["eth-mainnet"]
-  : ["eth-mainnet", "matic-mainnet", "arbitrum-mainnet", "base-mainnet"];
-const client = new CovalentClient(import.meta.env.VITE_COVALENT_API_KEY);
-
-const fetchTodayTokenBalances = async (address: string) => {
-  const allData = [];
-
-  for (const network of networks) {
-    try {
-      const resp = await client.BalanceService.getTokenBalancesForWalletAddress(
-        network as Chain,
-        address,
-        {"nft": false,"noSpam": true}
-      );
-      allData.push({ ...resp.data, chain_name: network });
-    } catch (error) {
-      console.error(`Failed to fetch data for network ${network}:`, error);
-    }
-  }
-
-  return allData;
-};
-
-const fetchPortfolioHistory = async (address: string) => {
-  const allData = [];
-
-  for (const network of networks) {
-    try {
-      const resp =
-        await client.BalanceService.getHistoricalPortfolioForWalletAddress(
-          network as Chain,
-          address
-        );
-      allData.push({ ...resp.data, chain_name: network });
-    } catch (error) {
-      console.error(`Failed to fetch data for network ${network}:`, error);
-    }
-  }
-
-  return allData;
-};
-
-const fetchChainList = async () => {
-  try {
-    const response = await fetch('https://api.0xsquid.com/v1/chains');
-    const allChains = (await response.json()).chains;
-
-    const fuseOptions = {
-      keys: [
-        {name: 'tags', weight: 1},
-        {name: 'chainName', weight: 0.5},
-        {name: 'networkName', weight: 0.5},
-      ],
-      threshold: 0.2
-    }
-    const fusedChains = new Fuse(allChains, fuseOptions)
-    console.log(fusedChains);
-    return(fusedChains);
-  } catch (error) {
-    console.error('Error fetching chain data:', error);
-    return [];
-  }
-}
-
-const fetchTokenList = async (chainId?: string | number) => {
-  try {
-    const response = await fetch( chainId ? `https://api.0xsquid.com/v1/tokens?chainId=${chainId}` : 'https://api.0xsquid.com/v1/tokens');
-    const allTokens = (await response.json()).tokens;
-    const fuseOptions = {
-      keys: [
-        {name: 'symbol', weight: 1},
-        {name: 'name', weight: 0.8},
-        {name: 'coingecko-id', weight: 0.5},
-      ],
-      threshold: 0.2
-    }
-    const fusedChains = new Fuse(allTokens, fuseOptions)
-    return(fusedChains);
-  } catch (error) {
-    console.error('Error fetching token data:', error);
-    return [];
-  }
-}
+import { useTokenBalances } from "./hooks/api";
+import { Markdown } from "./Components/Markdown";
 
 function bigIntReplacer(key, value) {
   if (typeof value === 'bigint') {
@@ -104,34 +19,7 @@ function bigIntReplacer(key, value) {
 const App = () => {
   const { address } = useAccount();
   const chatProvider = useChatHook();
-  const {
-    data: tokenBalances,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["tokenBalances", address],
-    queryFn: () => fetchTodayTokenBalances(address as string),
-    enabled: !!address,
-    staleTime: 1000 * 60 * 60, // 1 hour
-  });
-  const { data: portfolioHistory } = useQuery({
-    queryKey: ["portfolioHistory", address],
-    queryFn: () => fetchPortfolioHistory(address as string),
-    enabled: !!address,
-    staleTime: 1000 * 60 * 60, // 1 hour
-  });
-  const { data: searchableChains } = useQuery({
-    queryKey: ["chainList"],
-    queryFn: () => fetchChainList(),
-    staleTime: 1000 * 60 * 60 * 24 * 60, // 60 days
-  });
-  const { data: searchableTokens } = useQuery({
-    queryKey: ["tokenList"],
-    queryFn: () => fetchTokenList(),
-    staleTime: 1000 * 60 * 60 * 24 * 60, // 60 days
-  });
-  if (searchableChains) console.log(searchableChains.search('base')[0]);
-  if (searchableTokens) console.log(searchableTokens.search('usdc')[0]);
+  const { data: tokenBalances, isError, isLoading } = useTokenBalances(address || "");
 
   useEffect(() => {
     chatProvider.onCreateChat?.(chatProvider.DefaultPersonas[0]) // TODO : set personas
