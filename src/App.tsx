fbb35fe2876c80cf9dd65f9815260efcbe566aef
Bertrand <bertrandrigal.pro@gmail.com>
2024-06-01 14:05:27 +0200

    ✨ New squid button displayed inside the AI answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
-import { Chain, CovalentClient } from "@covalenthq/client-sdk";
 import TokenTable from "./Components/TokenTable"; // Import the TokenTable component
 import { useAccount } from "wagmi";
 import Loading from "./Components/Loading";
@@ -24,7 +22,16 @@ const App = () => {
   useEffect(() => {
     chatProvider.onCreateChat?.(chatProvider.DefaultPersonas[0]) // TODO : set personas
     if (!tokenBalances) return;
-    chatProvider.sendMessage(`Here is my portfolio details, do a wallet analysis and suggest some trades : ${JSON.stringify(
+    chatProvider.sendMessage(`Here is my portfolio details, do a very short wallet analysis and suggest me at least two or three trades to optimize my portfolio.
+    Use the following syntax to suggest a trade : [SQUID: fromChain: "<the chain name>", fromAmount: "<suggested amount in gwei or similar>", fromToken: "<the 'from' token symbol>", toChain: "<the chain name>", toToken: "<the 'to' token symbol>"]
+    
+    Important rules to follow:
+      - If there is no balance in the portfolio, suggest this hypothetical tx : [...] as an example, you could buy some Eth : [SQUID: fromChain: "ethereum", fromAmount: "10000000000000000", fromToken: "USDC", toChain: "ethereum", toToken: "eth"].
+      - Prefer trades that stay on the same chain to save on cross-chain fees, unless there's a significant advantage to moving across chains.
+      - Always return the [SQUID:...] syntax with an explanation a suggestion in your message.
+      - Always return the [SQUID:...] syntax as a new line but NEVER inside a code block.
+
+    Portfolio details : ${JSON.stringify(
           tokenBalances,
           bigIntReplacer)}`);
   }, [tokenBalances]);
--- src/Components/Markdown.tsx
+++ src/Components/Markdown.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import { ClassAttributes, Fragment, HTMLAttributes, useCallback, useState } from 'react'
+import React, { ClassAttributes, Fragment, HTMLAttributes, useCallback, useState } from 'react'
 import cs from 'classnames'
 import { RxClipboardCopy } from 'react-icons/rx'
 import ReactMarkdown, { ExtraProps } from 'react-markdown'
@@ -14,6 +12,9 @@ import remarkMath from 'remark-math'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import { useCopyToClipboard } from '../hooks/useCopyToClipboard'
+import { useChainList, useTokenList } from '../hooks/api'
+import SquidButton from './SquidButton'
+import { useAccount } from 'wagmi'
 
 export interface MarkdownProps {
   className?: string
@@ -61,6 +62,91 @@ const HighlightCode = (
   )
 }
 
+/**
+ * generateSquidConfigObject - This function generates the squid config object from the squid query string.
+ * The input has to follow this syntax : [SQUID: fromChain: "<the chain name>", fromAmount: "<suggested amount in gwei or similar>", fromToken: "<the 'from' token symbol>", toChain: "<the chain name>", toToken: "<the 'to' token symbol>"]
+ * Examples : 
+ * [SQUID: fromChain: "ethereum", fromAmount: "10000000000000000", fromToken: "USDC", toChain: "ethereum", toToken: "eth"]
+ * [SQUID: fromChain: "avalanche", fromAmount: "500000000000000000", fromToken: "AVAX", toChain: "polygon", toToken: "MATIC"]
+ * [SQUID: fromChain: "binance-smart-chain", fromAmount: "15000000000000000000", fromToken: "BNB", toChain: "cosmos", toToken: "Pyth"]
+ */
+const generateSquidConfigObject = (
+  squidQuery: string,
+  address: string
+) => {
+  if (!squidQuery.includes("[SQUID:")) {
+    throw new Error("Invalid Squid query");
+  }
+
+  const squidParams = squidQuery
+    .replace(/[\[\]]/g, '') // Remove the square brackets
+    .split("SQUID:")[1] // Split and take the part after "SQUID:"
+    .trim() // Remove leading/trailing whitespace
+    .split(",") // Split by comma to get individual parameters
+    .map((param) =>
+      param
+        .trim() // Remove leading/trailing whitespace from each parameter
+        .split(":") // Split by colon to separate key and value
+        .map((s) => s.trim().replace(/"/g, "")) // Remove quotes from each part
+    );
+
+  const squidObject = Object.fromEntries(
+    squidParams.map(([key, value]) => [key, value])
+  );
+  const { fromChain, fromAmount, fromToken, toChain, toToken } = squidObject;
+
+  const { data: searchableChains } = useChainList();
+  console.log(searchableChains);
+  const formattedFromChain = searchableChains?.search(fromChain)[0].item.chainId || fromChain;
+  const formattedToChain = searchableChains?.search(toChain)[0].item.chainId || toChain;
+  const { data: searchableFromTokens } = useTokenList(formattedFromChain);
+  const { data: searchableToTokens } = useTokenList(formattedToChain);
+  const formattedFromToken = searchableFromTokens?.search(fromToken)[0].item.address || fromToken;
+  const formattedToToken = searchableToTokens?.search(toToken)[0].item.address || toToken;
+
+  return {
+    fromChain: formattedFromChain,
+    fromAmount,
+    fromToken: formattedFromToken,
+    toChain: formattedToChain,
+    toToken: formattedToToken,
+    fromAddress: address,
+    toAddress: address,
+    bypassGuardRails: true,
+    slippageConfig: {
+      autoMode: 1,
+    }
+  };
+};
+
+const AddSquidButton = (
+  props: ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps
+) => {
+  const { children, className, ref, ...rest } = props
+  const match = /[\s\S]+SQUID:[\s\S]+/.exec(children || '')
+  const { address } = useAccount();
+
+  const squidButtonElement = React.Children.map(children, child => {
+    if (typeof child === 'string' && child.includes('SQUID:')) {
+      const squidObject = generateSquidConfigObject(child, address);
+      console.log('Generated squidConfig for SquidButton:', squidObject);
+
+      return <SquidButton squidConfig={squidObject} />;
+    }
+    return child;
+  });
+
+  return match ? (
+    <span ref={ref} {...rest} className={cs('squid-button-wrapper', className)}>
+      {squidButtonElement}
+    </span>
+  ):(
+    <span ref={ref} {...rest} className={className}>
+      {children}
+    </span>
+  );
+}
+
 export const Markdown = ({ className, children }: MarkdownProps) => {
   return (
     <ReactMarkdown
@@ -69,7 +155,14 @@ export const Markdown = ({ className, children }: MarkdownProps) => {
       rehypePlugins={[rehypeRaw, rehypeKatex, rehypeStringify]}
       components={{
         code(props) {
-          return <HighlightCode {...props} />
+          // return <HighlightCode {...props} />
+          return <AddSquidButton {...props} />
+        },
+        p(props) {
+          return <AddSquidButton {...props} />
+        },
+        span(props) {
+          return <AddSquidButton {...props} />
         }
       }}
     >
